ebb6ac5f2fb627b512163ecca75f3976654fd77c
Aleksandr.Bespalov <congenialen.gmail.com>
2023-08-31 21:54:04 +0400

    example 005 If Else

---
+++ example005IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string? username = Console.ReadLine();
+
+if(username.ToLower() == "sasha")
+{
+    Console.WriteLine("Ура, это же Sasha!");
+}
+else
+{
+       Console.Write("Привет, ");
+       Console.WriteLine(username);
+}
