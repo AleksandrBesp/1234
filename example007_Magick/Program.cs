f05f81db22e49167bb0e27182c2431e1c194eb57
Aleksandr.Bespalov <congenialen.gmail.com>
2023-09-04 21:07:17 +0400

    example 007 Magic

---
+++ example007_Magick/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+int xa = 5;
+int ya = 5;
+int xb = 5;
+int yb = 15;
+int xc = 40;
+int yc = 15;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
