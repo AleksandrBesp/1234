78ae80b2b8d69b542d021c49310f7e1cfa88ee91
Aleksandr.Bespalov <congenialen.gmail.com>
2023-09-05 21:00:20 +0400

    домашнее задание по нахожжению максимума из двух чисел

--- Seminar001Task2/Program.cs
+++ Seminar001Task2/Program.cs
@@ -3,11 +3,11 @@ int Num1 = Convert.ToInt32(Console.ReadLine());
 int Num2 = Convert.ToInt32(Console.ReadLine());
 if(Num1>Num2)
 {
-   Console.Write("Максимум"); 
+   Console.Write("Максимум: "); 
      Console.Write(Num1);
 }
 else
 {
-    Console.Write("Максимум"); 
+    Console.Write("Максимум: "); 
      Console.Write(Num2);
 }
