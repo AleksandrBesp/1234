575f9c4098081aadf96d291052ed668b79acae4d
Aleksandr.Bespalov <congenialen.gmail.com>
2023-09-05 20:54:53 +0400

    домашнее задание по нахожжению максимума из двух чисел

---
+++ Seminar001Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите числа для сравнения: ");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+int Num2 = Convert.ToInt32(Console.ReadLine());
+if(Num1>Num2)
+{
+   Console.Write("Максимум"); 
+     Console.Write(Num1);
+}
+else
+{
+    Console.Write("Максимум"); 
+     Console.Write(Num2);
+}
