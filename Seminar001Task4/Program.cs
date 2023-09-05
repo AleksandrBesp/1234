d5cd15b490a6dcc4e06d0da048177f6bfde637ef
Aleksandr.Bespalov <congenialen.gmail.com>
2023-09-05 22:22:46 +0400

    Homework. Maximum number of three.

---
+++ Seminar001Task4/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите три числа:");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+int Num2 = Convert.ToInt32(Console.ReadLine());
+int Num3 = Convert.ToInt32(Console.ReadLine());
+if(Num1 < Num2)
+{
+    if(Num1 > Num3)
+    {
+     Console.Write("Максимум: ");
+     Console.WriteLine(Num1);
+     }
+     else
+     {
+      Console.Write("Максимум: ");
+      Console.WriteLine(Num3);
+     }  
+}
+else
+{
+    if(Num2 > Num3)
+    {
+     Console.Write("Максимум: ");
+     Console.WriteLine(Num2);
+     }
+     else
+     {
+      Console.Write("Максимум: ");
+      Console.WriteLine(Num3);
+     }
+}
+
+    
+
+    
