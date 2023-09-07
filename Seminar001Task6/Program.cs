b4753c81c4dbe1557fb204872a535643cd6fa942
Aleksandr.Bespalov <congenialen.gmail.com>
2023-09-07 19:21:05 +0400

    Домашнее задание. Определение числа на чётное - не чётное.

---
+++ Seminar001Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+double Numb = Convert.ToInt32(Console.ReadLine());
+if(Numb % 2 == 0)
+{
+   Console.WriteLine("Число чётное.");
+}
+else
+{
+   Console.WriteLine("Число не чётное.");
+} 
