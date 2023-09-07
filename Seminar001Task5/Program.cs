f566cdfa05b54aabff4d1da61377234cd38d11a1
Aleksandr.Bespalov <congenialen.gmail.com>
2023-09-07 20:24:00 +0400

    Домашнее задание. Вывести все чётные числа до числа N.

--- Seminar001Task5/Program.cs
+++ Seminar001Task5/Program.cs
@@ -1,7 +1,6 @@
 ﻿Console.WriteLine("Введите число:");
 int numPositive = int.Parse(Console.ReadLine()??"0");
 int numNegative = -1 * numPositive;
-
 for (int i = numNegative; i < numPositive; i++)
 {
     Console.Write($"{i}, ");
---
+++ Seminar001Task8/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число:");
+int N = int.Parse(Console.ReadLine()??"0");
+for (int i = 2; i < N; i = i + 2)
+{
+    Console.Write($"{i}, ");
+}
