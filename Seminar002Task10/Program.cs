e9c66025984016585091f149914cc6231d86c011
Aleksandr.Bespalov <congenialen.gmail.com>
2023-09-10 15:56:33 +0400

    Добавил проверку на Трёхзначность числа

--- Seminar002Task10/Program.cs
+++ Seminar002Task10/Program.cs
@@ -1,9 +1,16 @@
 ﻿// Задание №10 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру.
 Console.Write("Введите трёхзначное число: ");
-int Numb = Convert.ToInt32(Console.ReadLine());
-
-int firstDigit = Numb/100;
-int lastDigit = Numb%10;
-Numb = (Numb - (firstDigit*100 + lastDigit))/10;
-
-Console.WriteLine(Numb);
+int Num2 = Convert.ToInt32(Console.ReadLine());
+int Num1 = Num2/100;
+int Num3 = Num2%10;
+int NumMax = 999;
+if(Num2>NumMax)
+{
+    Console.WriteLine("Число содержит больше трёх знаков!");
+}
+if(Num1 == 0)
+{
+    Console.WriteLine("Число содержит меньше трёх знаков!");
+}
+Num2 = (Num2 - (Num1*100 + Num3))/10;
+Console.WriteLine(Num2);
